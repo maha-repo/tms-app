1f90b00e12af7a8c2eb6bdaa414033c1dbf2dea6
maha-repo <mahadev.goje@gmail.com>
2015-07-29 01:41:46 -0700

    updated

--- src/com/vzw/hackathon/ActionClass.java
+++ src/com/vzw/hackathon/ActionClass.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
+
 import org.apache.struts.action.ActionMapping;
 
 /**
@@ -19,7 +20,7 @@ public class ActionClass extends Action{
 	
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
 	    ActionFormClass hwForm = (ActionFormClass) form;
-	    hwForm.setMessage("Hello World");
+	    hwForm.setMessage("Hello World...");
 	    return mapping.findForward("success");
 	    }
 
