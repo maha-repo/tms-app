43e3ce073e444645e1d36feaefd7748c968e9d2c
maha-repo <mahadev.goje@gmail.com>
2015-07-29 01:33:34 -0700

    Initial setup
    Initial setup

---
+++ src/com/vzw/hackathon/ActionClass.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.vzw.hackathon;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * @author Administrator
+ *
+ */
+public class ActionClass extends Action{
+	
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+	    ActionFormClass hwForm = (ActionFormClass) form;
+	    hwForm.setMessage("Hello World");
+	    return mapping.findForward("success");
+	    }
+
+
+}
---
+++ src/com/vzw/hackathon/ActionFormClass.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.vzw.hackathon;
+
+import org.apache.struts.action.ActionForm;
+
+/**
+ * @author Administrator
+ *
+ */
+public class ActionFormClass extends ActionForm{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	  private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+}
